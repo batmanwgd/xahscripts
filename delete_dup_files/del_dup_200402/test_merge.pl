56466bb5a6e2fe17dd9a675d26e77a1964b52c5f
Xah Lee <xah@xahlee.org>
2012-12-31 01:55:22 -0800

    routine update

--- delete_dup_files/del_dup_200402/test_merge.pl
+++
@@ -1,8 +0,0 @@
-
-use Combo114 qw(merge);
-
-use Data::Dumper;
-
-# print Dumper [merge([[3,4],[1,2],[5,4],[7,4]])];
-print Dumper [ merge( [[]] ) ];
-# print Dumper [ merge( [[],[3,5]] ) ];
--- delete_dup_files/del_dup_200402/x.py
+++
@@ -1,6 +0,0 @@
-#def reduce(n):
-#    return n+1
-
-def f (n,m):
-    return n+m
-print reduce (f, range(5))
