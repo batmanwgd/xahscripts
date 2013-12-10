ab6a2811741f8c364002947fb5d0783e4632730b
Xah Lee <xah@xahlee.org>
2013-12-09 23:49:25 -0800

    goodie

--- validate_website/check_local_links.pl
+++ validate_website/check_local_links.pl
@@ -21,7 +21,7 @@ use File::Basename;
 
 my $inDirPath = q{/home/xah/git/xah-emacs-tutorial/};
 my $inDirPath = q{/home/xah/web/};
-my $inDirPath = q{/home/xah/web/xahlee_org/diklo/xy_xah_emacs_tutorial/};
+# my $inDirPath = q{/home/xah/web/xahlee_org/diklo/xy_xah_emacs_tutorial/};
 
 my $webRootPath = q{/home/xah/web};
 
