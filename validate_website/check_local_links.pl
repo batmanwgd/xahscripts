792935c260097256c7bce2f0c92dec05cdcb4bc6
Xah Lee <xah@xahlee.org>
2014-02-10 11:39:22 -0800

    normal

--- validate_website/check_local_links.pl
+++ validate_website/check_local_links.pl
@@ -24,8 +24,8 @@ use File::Basename;
 my $userHomeDir = $ENV{"HOME"};
 my $webRootPath = qq{$userHomeDir/web};
 
+my $inDirPath = qq{$webRootPath/xahlee_info/comp};
 my $inDirPath = qq{$webRootPath};
-# my $inDirPath = qq{$webRootPath/xahlee_info/comp};
 
 $inDirPath = ($ARGV[0] ? $ARGV[0] : $inDirPath) ; # should give a full path; else the $File::Find::dir won't give full path.
 
