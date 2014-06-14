7bb814f247205eb607478a2e3de6cb6af00168f9
Xah Lee <xah@xahlee.org>
2014-06-14 03:48:30 -0700

    gone with the imp

--- update_ergoemacs-mode.sh
+++
@@ -1,4 +0,0 @@
-rm -r ~/git/ergoemacs/ergoemacs/ergoemacs-keybindings/
-cp -r ~/git/ergoemacs-mode/ ~/git/ergoemacs/ergoemacs/
-rm -r ~/git/ergoemacs/ergoemacs/ergoemacs-mode/.git
-mv ~/git/ergoemacs/ergoemacs/ergoemacs-mode ~/git/ergoemacs/ergoemacs/ergoemacs-keybindings
