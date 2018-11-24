c2387a8490eb0373f324ab3aea9f7e2e1bf09467
Xah Lee <xah@xahlee.org>
2018-11-24 01:08:54 -0800

    update

--- make_download_copy/make_download_copy_xahcode/make_download_copy_xahlee_info.rb
+++ make_download_copy/make_download_copy_xahcode/make_download_copy_xahlee_info.rb
@@ -2,8 +2,10 @@
 
 require 'fileutils'
 
-outpath ="/home/xah/web/xahlee_org/diklo/yy_xahlee_info"
-inpath ="/home/xah/web/xahlee_info"
+inpath ="/Users/xah/web/xahlee_info"
+
+outpath ="/Users/xah/web/xahlee_org/diklo/yy_xahlee_info"
+
 
 # remove existing
 if File.exist?(outpath)
@@ -19,9 +21,9 @@ then FileUtils.rm_rf(outpath + "/java8_doc")
 end
 
 # replace scripts and ads
-%x[python3 /home/xah/git/xahscripts/make_download_copy/find_replace_ads.py3 #{outpath}]
+%x[python3 /Users/xah/git/xahscripts/make_download_copy/find_replace_ads.py3 #{outpath}]
 
 # remove xx files and temp files etc
-%x[python3 /home/xah/git/xahscripts/make_download_copy/delete_temp_files.py3 #{outpath}]
+%x[python3 /Users/xah/git/xahscripts/make_download_copy/delete_temp_files.py3 #{outpath}]
 
 p 'done'
