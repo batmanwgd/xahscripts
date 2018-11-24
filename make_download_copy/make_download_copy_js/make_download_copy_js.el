556c33db6dd1a3dcf31c66d6be43f96b2da16eaa
Xah Lee <xah@xahlee.org>
2018-11-24 01:09:57 -0800

    update

--- make_download_copy/make_download_copy_js/make_download_copy_js.el
+++ make_download_copy/make_download_copy_js/make_download_copy_js.el
@@ -5,19 +5,15 @@
 (when (file-exists-p xxoutputpath) (delete-directory xxoutputpath "RECURSIVE" ) )
 
 (xah-make-downloadable-copy
- "/home/xah/web/xahlee_info/"
+ "~/web/xahlee_info/"
  [
-  "/home/xah/web/xahlee_info/js/"
-  "/home/xah/web/xahlee_info/javascript_ecma-262_5.1_2011/"
-  "/home/xah/web/xahlee_info/javascript_es6/"
-  "/home/xah/web/xahlee_info/javascript_es2016/"
-  "/home/xah/web/xahlee_info/html5_whatwg/"
-  "/home/xah/web/xahlee_info/dom-whatwg/"
-  "/home/xah/web/xahlee_info/jquery_doc/"
-  "/home/xah/web/xahlee_info/node_api/"
+  "~/web/xahlee_info/js/"
+  "~/web/xahlee_info/javascript_ecma-262_5.1_2011/"
+  "~/web/xahlee_info/javascript_es6/"
+  "~/web/xahlee_info/javascript_es2016/"
+  "~/web/xahlee_info/jquery_doc/"
+  "~/web/xahlee_info/node_api/"
   ]
  xxoutputpath)
 
-
 
-(xah-delete-files-by-regex xxoutputpath "^backbone")
--- make_download_copy/make_download_copy_js/make_download_copy_js.rb
+++ make_download_copy/make_download_copy_js/make_download_copy_js.rb
@@ -1,8 +1,8 @@
- # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 require 'fileutils'
 
-outpath ="/home/xah/web/xahlee_org/diklo/xx_xah_js_tutorial"
+outpath ="~/web/xahlee_org/diklo/xx_xah_js_tutorial"
 
 # replace scripts and ads
 %x[python3 ../find_replace_ads.py3 #{outpath}]
@@ -13,6 +13,6 @@ then FileUtils.remove_dir(outpath + "/js/blog.xml")
 end
 
 # remove xx files and temp files etc
-%x[python3 /home/xah/git/xahscripts/make_download_copy/delete_temp_files.py3 #{outpath}]
+%x[python3 ~/git/xahscripts/make_download_copy/delete_temp_files.py3 #{outpath}]
 
 # p 'done'
